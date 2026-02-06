2d617ba9c475664126324187cc5e5d5ead5a1a7f
Anil Deshpande <anildesh82@gmail.com>
2026-02-06 16:41:44 +0530

    AdBanner adding to ads module

---
+++ ads/src/main/java/com/codetutor/ads/AdBanner.kt
@@ -0,0 +1,23 @@
+package com.codetutor.ads
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AdBanner(modifier: Modifier = Modifier) {
+    Surface(
+        color = MaterialTheme.colorScheme.tertiaryContainer,
+        modifier = modifier
+    ) {
+        Text(
+            text = "AdBanner() from :ads module",
+            modifier = Modifier.padding(12.dp),
+            style = MaterialTheme.typography.bodyMedium
+        )
+    }
+}
